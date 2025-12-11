c4b75ca4b1dd1831fd9ffc150b2610f1aaa99069
anastasiaanaya <anastasia.anaya@estudiantat.upc.edu>
2025-12-11 14:04:08 +0100

    cosas cosas cositass

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,18 +1,43 @@
 #include "ofApp.h"
 
+
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+	ofBackground(0);
+	server.setup(12345); //the same port as in Python
+	server.setMessageDelimiter("\n");
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+	if (server.isConnected())
+	{
+		string msg = server.getLastMessage();
+		if (!msg.empty())
+		{
+			vector<ofPoint> points;
+			stringstream ss(msg); //buscar para que es el ss!!
+			string token;
+			while (getline(ss, token, ';'))
+			{
+				stringstream coord(token);
+				float x, y;
+				char comma;
+				coord >> x >> comma >> y;
+				//scale to the window's setup
+				points.push_back(ofPoint(x * ofGetWidth(), y * ofGetHeight()));
+			}
+			handPoints = points;
+		}
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+	ofSetColor(0, 255, 0);
+	for (auto & p : handPoints) {
+		ofDrawCircle(p, 5);
+	}
 }
 
 //--------------------------------------------------------------
